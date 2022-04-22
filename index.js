2db87b887d0d51a8666cabdd1876b47cb917988f
ChinaevVladimir <chinaev.exceedteam@gmail.com>
2022-04-22 15:31:14 +0300

    make code refactoring

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-const Schema = mongoose.Schema;
+const cors = require("cors");
 app.use(express.json());
 
-const taskScheme = new Schema({
-  text: String,
-  isCheck: Boolean,
-});
+const apiRoutes = require("./src/modules/routes/routes");
+
+app.use(cors());
 
 const uri =
   "mongodb+srv://root:root@cluster0.9u3oi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(uri, {});
 
-const Task = mongoose.model("tasks", taskScheme);
-
-app.get("/allTasks", (req, res) =>
-  Task.find().then((result) => res.send({ data: result }))
-);
-
-app.post("/createTasks", (req, res) => {
-  const task = new Task(req.body);
-  task.save().then((result) => res.send(result));
-});
-
-app.patch("/updateTask", (req, res) => {
-  Task.updateOne({ _id: req.query.id }, req.body).then((result) =>
-    Task.find({ _id: req.body.id }).then((result) => res.send(result))
-  );
-});
-
-app.delete("/deleteTasks", (req, res) =>
-  Task.deleteOne({ _id: req.query.id }).then((result) => res.send(result))
-);
+app.use(bodyParser.json());
+app.use("/", apiRoutes);
 
 app.listen(7070, () => console.log("Example app listening on port 7070!"));
---
+++ src/modules/controllers/taskController.js
@@ -0,0 +1,37 @@
+const Task = require("../../db/model/task/index");
+
+module.exports.getAllTasks = (req, res) =>
+  Task.find().then((result) => res.send({ data: result }));
+
+module.exports.createNewTask = (req, res) => {
+  const body = req.body;
+  if (!(body.hasOwnProperty("text") && body.hasOwnProperty("isCheck"))) {
+    res.send("add text or isCheck please!");
+  } else {
+    const task = new Task(req.body);
+    task.save().then((result) => res.send(result));
+  }
+};
+
+module.exports.changeTaskInfo = (req, res) => {
+  const body = req.body;
+  if (
+    !(
+      body.hasOwnProperty("id") &&
+      (body.hasOwnProperty("text") || body.hasOwnProperty("isCheck"))
+    )
+  ) {
+    res.send("add value please!");
+  } else {
+    Task.updateOne({ _id: req.query.id }, req.body).then((result) =>
+      Task.find({ _id: req.body.id }).then((result) => res.send(result))
+    );
+  }
+};
+
+module.exports.daleteTask = (req, res) => {
+  if (!req.query.id) return res.status(422).send("Error! Params not correct");
+  else {
+    Task.deleteOne({ _id: req.query.id }).then((result) => res.send(result));
+  }
+};
